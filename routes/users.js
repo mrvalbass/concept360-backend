403931cd41ae6f09407067b731630a457bd4397c
Ines L <ines.latrache@gmail.com>
2024-05-27 15:09:19 +0200

    replace relative URL to absolute

--- routes/users.js
+++ routes/users.js
@@ -173,9 +173,10 @@ router.put("/specialists/deletePatient", async (req, res) => {
 });
 
 router.post("/upload", async (req, res) => {
-  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const photoPath = `/tmp/${uniqid()}.jpg`;
   const resultMove = await req.files.photoFromFront.mv(photoPath);
-
+  //console.log(req.files.photoFromFront);
+  console.log(resultMove);
   if (!resultMove) {
     const resultCloudinary = await cloudinary.uploader.upload(photoPath);
     res.json({ result: true, url: resultCloudinary.secure_url });
