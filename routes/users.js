8a6c7257a85c4c7ac828a2f235fa055ae26bc41e
Ines L <ines.latrache@gmail.com>
2024-05-27 13:33:34 +0200

    ajout route upload cloudiany et get profil

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cloudinary = require("cloudinary").v2;
 const uid2 = require("uid2");
+const fs = require("fs");
+const uniqid = require("uniqid");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../models/users");
 const Specialist = require("../models/specialists");
 const Patient = require("../models/patients");
+const mongoose = require("mongoose");
 
 /* GET users listing. */
 router.get("/", async (req, res) => {
@@ -27,7 +31,7 @@ router.get("/state/:state", async (req, res) => {
       const listSpecialist = await Specialist.find();
       res.json({ result: true, Specialist: listSpecialist });
     } else if (req.params.state === "patient") {
-      const listPatient = await Patient.find();
+      const listPatient = await Patient.find().populate("user");
       res.json({ result: true, Patient: listPatient });
     }
   } catch (err) {
@@ -35,10 +39,15 @@ router.get("/state/:state", async (req, res) => {
   }
 });
 
-router.get("/getPatient/:specialistId", async (req, res) => {
+router.get("/getPatientList/:specialistId", async (req, res) => {
   try {
-    const specialist = await Specialist.findById(req.params.specialistId);
-    res.json({ result: true, listPatient: specialist.patients });
+    const specialist = await Specialist.findById(
+      req.params.specialistId
+    ).populate({
+      path: "patients",
+      populate: "user",
+    });
+    res.json({ result: true, PatientList: specialist.patients });
   } catch (err) {
     res.json({ result: false, error: err.message });
   }
@@ -48,7 +57,12 @@ router.get("/token/:token", async (req, res) => {
   try {
     if (!req.params.token) throw new Error("no token provided");
     const user = await User.findOne({ token: req.params.token });
-    res.json({ result: true, user });
+    const specialist = await Specialist.findOne({ user: user._id }).populate(
+      "user"
+    );
+    const patient = await Patient.findOne({ user: user._id }).populate("user");
+
+    res.json({ result: true, specialist, patient });
   } catch (err) {
     res.json({ result: false, error: err.message });
   }
@@ -56,6 +70,10 @@ router.get("/token/:token", async (req, res) => {
 
 router.post("/signup", async (req, res) => {
   try {
+    if (!req.body.state) throw new Error("no state provided");
+    const user = await User.findOne({ email: req.body.email });
+    if (user) throw new Error("User already exist");
+
     const newUser = await new User({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -65,13 +83,13 @@ router.post("/signup", async (req, res) => {
     }).save();
 
     if (req.body.state === "specialist") {
-      const newSpecialist = await new Specialist({
+      if (!req.body.discipline) throw new Error("no discipline provided");
+      await new Specialist({
         user: newUser._id,
         discipline: req.body.discipline,
       }).save();
     }
     if (req.body.state === "patient") {
-      console.log(newUser._id);
       await new Patient({
         user: newUser._id,
       }).save();
@@ -94,4 +112,81 @@ router.post("/signin", async (req, res) => {
   }
 });
 
+router.post("/addPatient/:specialistId", async (req, res) => {
+  try {
+    await Patient.updateOne(
+      { _id: req.body.patientId },
+      { $push: { specialists: req.params.specialistId } }
+    );
+    await Specialist.updateOne(
+      { _id: req.params.specialistId },
+      { $push: { patients: req.body.patientId } }
+    );
+    res.json({ result: true });
+  } catch (err) {
+    res.json({ result: false, error: err.message });
+  }
+});
+
+router.delete("/deletePatient/:specialistId", async (req, res) => {
+  try {
+    const patientId = new mongoose.Types.ObjectId(`${req.body.patientId}`);
+
+    await Patient.updateOne(
+      {
+        _id: patientId,
+      },
+      {
+        $pull: { specialists: req.params.specialistId },
+      }
+    );
+
+    await Specialist.updateOne(
+      {
+        _id: req.params.specialistId,
+      },
+      {
+        $pull: { patients: patientId },
+      }
+    );
+
+    res.json({ result: true });
+  } catch (err) {
+    res.json({ result: false, error: err.message });
+  }
+});
+
+// route pour envoyer et récuperer les photos dans cloundinary
+
+router.post("/upload", async (req, res) => {
+  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const resultMove = await req.files.photoFromFront.mv(photoPath);
+
+  if (!resultMove) {
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    res.json({ result: true, url: resultCloudinary.secure_url });
+  } else {
+    res.json({ result: false, error: resultMove });
+  }
+
+  fs.unlinkSync(photoPath);
+});
+
+router.get("/getProfil", async (req, res) => {
+  try {
+    // Récupérer les photos de profil depuis Cloudinary
+    const profileImages = await cloudinary.v2.uploader
+      .explicit("sample", { type: "image/jpeg" })
+      .then((result) => console.log(result));
+    res.json({ profileImages });
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération des photos de profil depuis Cloudinary:",
+      error
+    );
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des photos de profil" });
+  }
+});
 module.exports = router;
